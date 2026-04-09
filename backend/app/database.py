8c0a94c478de353cf87d8b3860d03aa4a02e5386
shwetasaw24 <shwetasaw24@gmail.com>
2026-04-10 05:03:25 +0530

    ready for cloud

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,11 +4,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"sqlite:///{BASE_DIR}/test.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+#  fallback for local development
+if not DATABASE_URL:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATABASE_URL = f"sqlite:///{BASE_DIR}/test.db"
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    #  required for Render PostgreSQL
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
